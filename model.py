e7231985cbdfd8dba2e92548786ec787602134ae
Aditya Ponde <94423956+adiponde22@users.noreply.github.com>
2023-09-25 12:36:10 -0400

    Add files via upload

--- model.py
+++ model.py
@@ -10,45 +10,62 @@ from sklearn.metrics import mean_squared_error
 
 # Function to train a stock price predictor for a given stock symbol using polynomial regression
 def train_stock_price_predictor(symbol, degree=2, period="max"):
-    # Download the historical data for the given stock symbol
-    stock = yf.Ticker(symbol)
-    stock_data = stock.history(period=period)
+    try:
+        # Download the historical data for the given stock symbol
+        stock = yf.Ticker(symbol)
+        stock_data = stock.history(period=period)
 
-    # Ensure that the stock data contains the necessary columns (Open, Close, High, Low, Volume)
-    if 'Open' not in stock_data.columns or 'Close' not in stock_data.columns \
-        or 'High' not in stock_data.columns or 'Low' not in stock_data.columns \
-        or 'Volume' not in stock_data.columns:
-        raise ValueError("Stock data is missing required columns (Open, Close, High, Low, Volume).")
+        # Ensure that the stock data contains the necessary columns (Open, Close, High, Low, Volume)
+        if 'Open' not in stock_data.columns or 'Close' not in stock_data.columns \
+            or 'High' not in stock_data.columns or 'Low' not in stock_data.columns \
+            or 'Volume' not in stock_data.columns:
+            raise ValueError("Stock data is missing required columns (Open, Close, High, Low, Volume).")
 
-    # Shift the opening price data to predict the future opening price (next day)
-    stock_data['Future Open'] = stock_data['Open'].shift(-1)
+        # Shift the opening price data to predict the future opening price (next day)
+        stock_data['Future Open'] = stock_data['Open'].shift(-1)
 
-    # Drop rows with NaN values (last rows with no future data)
-    stock_data = stock_data.dropna()
+        # Drop rows with NaN values (last rows with no future data)
+        stock_data = stock_data.dropna()
 
-    # Split the data into features (X) and target (y)
-    X = stock_data[['Open', 'Close', 'High', 'Low', 'Volume']]
-    y = stock_data['Future Open']
+        # Split the data into features (X) and target (y)
+        X = stock_data[['Open', 'Close', 'High', 'Low', 'Volume']]
+        y = stock_data['Future Open']
 
-    # Create a polynomial regression model
-    model = make_pipeline(PolynomialFeatures(degree), LinearRegression())
+        # Create a polynomial regression model
+        model = make_pipeline(PolynomialFeatures(degree), LinearRegression())
 
-    # Fit the polynomial regression model to the data
-    model.fit(X, y)
+        # Fit the polynomial regression model to the data
+        model.fit(X, y)
 
-    return model
+        return model
+
+    except Exception as e:
+        st.error(f"Error: INVALID TICKER")
+        return None
+
+
+
 # Function to predict the future opening price of a stock using a trained model
 def predict_future_opening_price(model, symbol, historical_data):
-    # Extract the feature values (Open, Close, High, Low, Volume) for the last data point
-    latest_features = historical_data[['Open', 'Close', 'High', 'Low', 'Volume']].iloc[-1].values
+    try:
+        if historical_data.empty:
+            st.error("ENTER A STOCK LISTED ON THE NASDAQ")
+            return None
 
-    # Reshape the features to have the shape (1, 5)
-    latest_features = latest_features.reshape(1, -1)
+        # Extract the feature values (Open, Close, High, Low, Volume) for the last data point
+        latest_features = historical_data[['Open', 'Close', 'High', 'Low', 'Volume']].iloc[-1].values
 
-    # Use the trained model to make predictions on the latest data
-    predicted_opening_price = model.predict(latest_features)[0]
+        # Reshape the features to have the shape (1, 5)
+        latest_features = latest_features.reshape(1, -1)
 
-    return predicted_opening_price
+        # Use the trained model to make predictions on the latest data
+        predicted_opening_price = model.predict(latest_features)[0]
+
+        return predicted_opening_price
+
+    except Exception as e:
+        st.error(f"Error: {e}")
+        return None
 
 
 def make_recommendation(predicted_opening_price, current_closing_price):
